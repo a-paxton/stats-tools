4c04c4668121cc23e3225b58a707d92ceeca276c
Alexandra Paxton <paxton.alexandra@gmail.com>
2016-10-04 17:12:35 -0700

    Add cleaner significance displays and switch to MuMIn for R-squared

--- pander_lme.R
+++ pander_lme.R
@@ -1,18 +1,30 @@
 # 'pander_lme': Create a cleaner lme4 model output with pander.
 #
-# Optional (requires 'piecewiseSEM'): 
+# Optional (requires 'MuMIn'): 
 #   Include caption with marginal (i.e., fixed effects only) 
 #   and conditional (i.e., fixed and random effects) R-squared.
 
 pander_lme = function(lme_model_name, stats.caption){
-  library(pander)
+  
+  # load in pander
+  require(pander)
+  
+  # disable scientific notation
+  options(scipen = 999)
   
   # convert the model summary to a dataframe
   neat_output = data.frame(summary(lme_model_name)$coefficient)
   
-  # get it to display p-values and asterisks based on significance
+  # round p-values (using Psychological Science's recommendations) for display
   neat_output$p = 2*(1-pnorm(abs(neat_output$t.value)))
+  neat_output$p[neat_output$p < .0001] = .0001
+  neat_output$p[neat_output$p >= .0001] = round(neat_output$p[neat_output$p >= .0001],4)
+  neat_output$p[neat_output$p >= .0005] = round(neat_output$p[neat_output$p >= .0005],3)
+  neat_output$p[neat_output$p >= .25] = round(neat_output$p[neat_output$p >= .25],2)
+  
+  # create significance and trending markers
   neat_output$sig = ' '
+  neat_output$sig[neat_output$p < .1] = '.'
   neat_output$sig[neat_output$p < .05] = '*'
   neat_output$sig[neat_output$p < .01] = '**'
   neat_output$sig[neat_output$p < .001] = '***'
@@ -20,26 +32,19 @@ pander_lme = function(lme_model_name, stats.caption){
   # if desired, create a caption that includes R-squared
   if (stats.caption == TRUE){
     
-    # use piecewiseSEM to calculate R-squared
-    library(piecewiseSEM)
-    
-    # get both marginal and conditional R-squared values
-    model_marginal_r_squared = sem.model.fits(lme_model_name)$Marginal
-    model_conditional_r_squared = sem.model.fits(lme_model_name)$Conditional
-    
-    # create caption (includes markdown formatting)
+    # use MuMIN to calculate R-squared
+    require(MuMIn)
+    model_marginal_r_squared = r.squaredGLMM(lme_model_name)[['R2m']]
+    model_conditional_r_squared = r.squaredGLMM(lme_model_name)[['R2c']]
     neat_caption = paste('**Marginal *R*-squared: ',
                          round(model_marginal_r_squared,2), 
-                         "; Conditional *R*-squared: ",
-                         round(model_conditional_r_squared,2), ".**",sep="")
+                         ". Conditional *R*-squared: ",
+                         round(model_conditional_r_squared,2),".**",sep="")
     
-    # return the table with caption
-    return(pander(neat_output, split.table = Inf, caption = neat_caption))
+    # return the table
+    return(pander(neat_output, split.table = Inf, caption = neat_caption, style = 'rmarkdown'))
     
-  } else {
-    
-    # or return a table without it
-    return(pander(neat_output, style="rmarkdown",split.table = Inf))
-    
+  } else { # or return a table without it
+    return(pander(neat_output, split.table = Inf, style = 'rmarkdown'))
   }
 }
